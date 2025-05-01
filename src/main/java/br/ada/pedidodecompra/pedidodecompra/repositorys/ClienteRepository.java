04110ba9121ec4f892166f9c09243a6fb6fc1c10
PedroNeves <pedronevesbezerra@gmail.com>
2025-05-01 14:47:58 -0300

    add method in cliente repository

--- src/main/java/br/ada/pedidodecompra/pedidodecompra/repositorys/ClienteRepository.java
+++ src/main/java/br/ada/pedidodecompra/pedidodecompra/repositorys/ClienteRepository.java
@@ -3,5 +3,9 @@ package br.ada.pedidodecompra.pedidodecompra.repositorys;
 import br.ada.pedidodecompra.pedidodecompra.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Optional<Cliente> findByEmail(String email);
 }
--- src/main/java/br/ada/pedidodecompra/pedidodecompra/repositorys/ItemPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.ada.pedidodecompra.pedidodecompra.repositorys;
-
-import br.ada.pedidodecompra.pedidodecompra.entities.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
-}
